9310fdfb78a5d1439ee59b759f951f7d68a62c00
Gavy Aggarwal <gavyaggarwal@gmail.com>
2017-03-09 22:56:27 -0800

    Added remaining visualizations

--- visualizations.py
+++ visualizations.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import pickle
+import seaborn as sns
 
 with open('data/movie_info.pickle', 'rb') as handle:
     movieInfo = pickle.load(handle)
 
+every = range(len(movieInfo))
 random = [0, 120, 255, 49, 21, 1479, 1469, 1, 1273, 448]
 popular = np.argsort([len(movie['ratings']) for movie in movieInfo])[-10:]
 best = np.argsort([np.mean(movie['ratings']) for movie in movieInfo])[-10:]
@@ -12,6 +15,33 @@ dramas = [i for i, m in enumerate(movieInfo) if "Drama" in m["genre"]]
 horrors = [i for i, m in enumerate(movieInfo) if "Horror" in m["genre"]]
 actions = [i for i, m in enumerate(movieInfo) if "Action" in m["genre"]]
 
+def makeHistogram(movies, filename, title, aggregate=True):
+    ratings = []
+    titles = []
+    for i in movies:
+        for rating in movieInfo[i]["ratings"]:
+            ratings.append(rating)
+            titles.append(movieInfo[i]["title"])
+
+    data = {'ratings' : ratings, 'titles' : titles}
+
+    fig, ax = plt.subplots()
+
+    if aggregate:
+        ax = sns.countplot(data=data, x='ratings')
+        ax.set_xlabel("Rating")
+    else:
+        ax = sns.countplot(data=data, x='titles', hue='ratings')
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+        ax.set_xlabel("Movie Title")
+
+    ax.set_ylabel("Frequency")
+    ax.set_title("Distribution of Ratings for " + title)
+
+    fig.tight_layout()
+
+    plt.savefig("img/histogram_" + filename)
+    plt.clf()
 
 def makeScatterPlot(movies, filename, title):
     x = [movieInfo[i]["coords"][0] for i in movies]
@@ -25,28 +55,27 @@ def makeScatterPlot(movies, filename, title):
     ax.scatter(x, y)
     for i, j in enumerate(movies):
         ax.annotate(movieInfo[j]["title"], (x[i] + 0.015, y[i] - 0.015))
-    fig.savefig("img/" + filename)
+    fig.savefig("img/scatter_" + filename)
     fig.clf()
 
 def part1():
-    pass
+    makeHistogram(every, "all.png", "All Movies")
+    makeHistogram(dramas, "drama.png", "Drama Movies")
+    makeHistogram(horrors, "horror.png", "Horror Movies")
+    makeHistogram(actions, "action.png", "Action Movies")
+    makeHistogram(popular, "popular.png", "10 Most Popular Movies", aggregate=False)
+    makeHistogram(best, "best.png", "10 Highest Rated Movies", aggregate=False)
 
 def part2():
-    makeScatterPlot(random, "random_scatter.png", "10 Random Movies")
-    makeScatterPlot(popular, "popular_scatter.png", "10 Most Popular Movies")
-    makeScatterPlot(best, "best_scatter.png", "10 Highest Rated Movies")
-    makeScatterPlot(dramas[:10], "drama_scatter.png", "10 Drama Movies")
-    makeScatterPlot(horrors[:10], "horror_scatter.png", "10 Horror Movies")
-    makeScatterPlot(actions[:10], "action_scatter.png", "10 Action Movies")
+    makeScatterPlot(random, "random.png", "10 Random Movies")
+    makeScatterPlot(popular, "popular.png", "10 Most Popular Movies")
+    makeScatterPlot(best, "best.png", "10 Highest Rated Movies")
+    makeScatterPlot(dramas[:10], "drama.png", "10 Drama Movies")
+    makeScatterPlot(horrors[:10], "horror.png", "10 Horror Movies")
+    makeScatterPlot(actions[:10], "action.png", "10 Action Movies")
 
-def freqAll():
-    n, bins, patches = plt.hist(ratings, bins=[1,2,3,4,5,6], align='left')
-    plt.title("Distribution of Ratings")
-    plt.xlabel("Rating")
-    plt.ylabel("Frequency")
-    plt.savefig('ratingsFreq.png')
-    plt.clf()
-
+def old():
+    # I moved all the old code here
     labels = ['1', '2', '3', '4', '5']
     colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', '#cc99ff']
     explode = (0, 0, 0, 0, 0)
@@ -56,7 +85,6 @@ def freqAll():
     plt.savefig('ratingsPie.png')
     plt.clf()
 
-def freqPopular():
     numRatings = []
     for movie in popular:
         numRatings.append(movieInfo[movie]['ratings'])
@@ -68,12 +96,9 @@ def freqPopular():
     plt.savefig('popularFreq.png')
     plt.clf()
 
-def freqBest():
     print("BEST RATED")
     for movie in best:
         print(movieInfo[movie]['title'], np.mean(movieInfo[movie]['ratings']), len(movieInfo[movie]['ratings']))
 
-freqPopular()
-freqBest()
-
-part1() and part2()
+part1()
+part2()
